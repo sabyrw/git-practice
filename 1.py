e2e5623798fe971c2a723b420fcf1e62c20d7cd7
sabyrw <bakdaulet.sabyr007@gmail.com>
2026-02-02 09:37:26 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ 1.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+episodes = {}
+
+for _ in range(n):
+    s, k = input().split()
+    k = int(k)
+    if s in episodes:
+        episodes[s] += k
+    else:
+        episodes[s] = k
+
+# Print doramas in lexicographical order
+for dorama in sorted(episodes):
+    print(dorama, episodes[dorama])
