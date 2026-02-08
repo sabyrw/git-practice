b18984e6c7734e088bd4fe7a36e78768ac40495e
sabyrw <bakdaulet.sabyr007@gmail.com>
2026-02-08 18:41:49 +0500

    Add Practice 3(Python functions, lambda, classes, inherintance)

---
+++ practice3/README.md
@@ -0,0 +1,9 @@
+# Practice-03
+
+Python practice project covering:
+- Functions
+- Lambda functions
+- Classes
+- Inheritance
+
+Each folder contains simple examples for learning core Python concepts.
---
+++ practice3/classes/class_dlte.py
@@ -0,0 +1,11 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Linus", 30)
+
+del p1.age
+
+print(p1.name) # This works
+# print(p1.age) # This would cause an error
